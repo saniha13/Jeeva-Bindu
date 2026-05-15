805ac5eb09111435935425f1e37211f421cbebbd
saniha13 <saniharbangera@gmail.com>
2026-05-15 19:13:14 +0530

    Add color constants for Jeeva app

--- utils/Constants.kt
+++ utils/Constants.kt
@@ -1,1 +1,7 @@
+package com.example.jeeva_bindu.utils
 
+import androidx.compose.ui.graphics.Color
+
+val JeevaRed = Color(0xFFD32F2F)
+val JeevaGreen = Color(0xFF2E7D32)
+val JeevaGray = Color(0xFFF5F5F5)
